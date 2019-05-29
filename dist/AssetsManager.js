d1a96063576ff14f81935a148013b60b1de12379
dungeon2567 <diegobguedes@gmail.com>
2019-05-29 12:26:53 -0300

    gcloud

--- dist/AssetsManager.js
+++ dist/AssetsManager.js
@@ -1,8 +1,8 @@
 
 var AssetsManager = function() {
     this.cacheName = 'appCache'; 
-    this.cacheEntries = ['/f9270d2ead3648aa6159.hot-update.json','/img/logo.404c148a.png','/0.js','/1.js','/2.js','/app.js','/e37233a7f580837a311d.hot-update.json','/index.html','/service-worker.js','/','/other'];
-    this.hashes = ['85e3aa8d5f5104f4ebaf','0','1','2','app','index','service-worker'];
+    this.cacheEntries = ['/img/logo.404c148a.png','/css/app.e234c611.css','/js/app.d1fc18d5.js','/css/chunk-0de5bf28.1399107e.css','/js/chunk-0de5bf28.b198d861.js','/js/chunk-2d0a447c.0c25877d.js','/js/chunk-2d0e613e.286b9177.js','/css/chunk-vendors.391ed6f9.css','/js/chunk-vendors.05bfbc65.js','/index.html','/img/icons/apple-touch-icon-120x120.png','/img/icons/apple-touch-icon-152x152.png','/img/icons/apple-touch-icon-180x180.png','/img/icons/apple-touch-icon-60x60.png','/img/icons/apple-touch-icon-76x76.png','/img/icons/apple-touch-icon.png','/img/icons/favicon-16x16.png','/img/icons/favicon-32x32.png','/manifest.json','/robots.txt','/img/icons/android-chrome-192x192.png','/img/icons/msapplication-icon-144x144.png','/favicon.ico','/img/icons/mstile-150x150.png','/img/icons/android-chrome-512x512.png','/service-worker.js','/img/icons/safari-pinned-tab.svg','/','/other'];
+    this.hashes = ['ce54aa53436ab71a5fbd','index','img/icons/apple-touch-icon-120x120','img/icons/apple-touch-icon-152x152','img/icons/apple-touch-icon-180x180','img/icons/apple-touch-icon-60x60','img/icons/apple-touch-icon-76x76','img/icons/apple-touch-icon','img/icons/favicon-16x16','img/icons/favicon-32x32','manifest','robots','img/icons/android-chrome-192x192','img/icons/msapplication-icon-144x144','favicon','img/icons/mstile-150x150','img/icons/android-chrome-512x512','service-worker','img/icons/safari-pinned-tab'];
 };
 
 AssetsManager.prototype.addAllToCache = function() {
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,27 +1,23 @@
-try {
-  importScripts("/AssetsManager.js");
+importScripts("/AssetsManager.js");
 
-  let assetsManager = new AssetsManager(); //create an instance of AssetsManager
+let assetsManager = new AssetsManager(); //create an instance of AssetsManager
 
-  self.addEventListener("install", event => {
-    event.waitUntil(assetsManager.addAllToCache());
-  });
+self.addEventListener("install", event => {
+  event.waitUntil(assetsManager.addAllToCache());
+});
 
-  self.addEventListener("activate", event => {
-    event.waitUntil(assetsManager.removeNotInAssets());
-  });
+self.addEventListener("activate", event => {
+  event.waitUntil(assetsManager.removeNotInAssets());
+});
 
-  self.addEventListener("fetch", function(event) {
-    if (event.request.method !== "GET") {
-      return;
-    }
-    // Cache then network
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
-} catch (ex) {
-  console.log(ex);
-}
+self.addEventListener("fetch", function(event) {
+  if (event.request.method !== "GET") {
+    return;
+  }
+  // Cache then network
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
